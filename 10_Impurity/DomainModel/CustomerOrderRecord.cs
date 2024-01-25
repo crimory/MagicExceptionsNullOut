a80b62b404e3bb3f8b01f4343634c8b83c922ff9
Marcin Kern <silonpl@gmail.com>
2024-01-25 08:43:11 +0100

    complete impurity vs records

---
+++ 10_Impurity/DomainModel/CustomerOrderRecord.cs
@@ -0,0 +1,41 @@
+using _10_Impurity.Errors;
+
+namespace _10_Impurity.DomainModel;
+
+public record CustomerOrderRec(
+    string Number,
+    CustomerOrderPositionRec[] Positions,
+    CustomerRec Customer);
+
+public record CustomerOrderPositionRec(
+    string ItemName,
+    uint Quantity);
+
+public record CustomerRec(
+    string Name);
+
+public static class RecordLogUsage
+{
+    private static void LogUsageExample()
+    {
+        var order = new CustomerOrderRec("123",
+            [
+                new CustomerOrderPositionRec("Item 1", 5),
+                new CustomerOrderPositionRec("Item 2", 3)
+            ],
+            new CustomerRec("John Doe"));
+        var orderOrError = RailwayUtility.RailwayBind(order);
+        LogInfo(orderOrError);
+    }
+    
+    private static void LogInfo(ErrorOrOutput<CustomerOrderRec> orderOrError)
+    {
+        _ = orderOrError.Match(
+            order =>
+            {
+                // order.Customer.Name = "Captain Pollution was here!";
+                return true;
+            },
+            _ => true);
+    }
+}
